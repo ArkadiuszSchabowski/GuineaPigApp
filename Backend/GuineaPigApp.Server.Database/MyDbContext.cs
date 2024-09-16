76b6247c8c5675629672d306b7d0f098d57b49d2
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-16 02:00:57 +0200

    Add initial setup and references for GuineaPigApp.Server.Database project

---
+++ Backend/GuineaPigApp.Server.Database/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuineaPigApp.Server.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Backend/GuineaPigApp.Server/Program.cs
+++ Backend/GuineaPigApp.Server/Program.cs
@@ -1,8 +1,21 @@
+using GuineaPigApp.Server.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MyDbContext>(options => options.UseInMemoryDatabase("MemoryDb"));
+}
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionDbConnectionString")));
+}
 
 var app = builder.Build();
 
@@ -14,6 +27,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
