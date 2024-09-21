7b937190b7e7cbc0c693fd8c669d4e83173b8da5
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-21 19:26:06 +0200

    Implement repository pattern to Product

---
+++ Backend/GuineaPigApp.Server/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GuineaPigApp.Server.Database.Entities;
+
+namespace GuineaPigApp.Server.Interfaces
+{
+    public interface IProductRepository
+    {
+        void AddProduct(Product product);
+        List<Product> GetBadProducts();
+        List<Product> GetGoodProducts();
+        Product? GetProduct(int id);
+        void RemoveProduct(Product product);
+        void SaveChanges();
+    }
+}
---
+++ Backend/GuineaPigApp.Server/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using GuineaPigApp.Server.Database;
+using GuineaPigApp.Server.Database.Entities;
+using GuineaPigApp.Server.Interfaces;
+
+namespace GuineaPigApp.Server.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MyDbContext _context;
+
+        public ProductRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+        public List<Product> GetBadProducts()
+        {
+            return _context.Products
+                     .Where(x => x.isGoodProduct == false)
+                     .ToList();
+        }
+        public List<Product> GetGoodProducts()
+        {
+            return _context.Products
+         .Where(x => x.isGoodProduct == true)
+         .ToList();
+        }
+        public Product? GetProduct(int id)
+        {
+            return _context.Products.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            _context.Products.Remove(product);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
