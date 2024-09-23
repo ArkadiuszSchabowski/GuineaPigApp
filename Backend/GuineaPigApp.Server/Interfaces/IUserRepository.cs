dc657a5dab11a0c11c8721f15e16abbc29b48c04
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-23 21:17:46 +0200

    Add Account components: Controller, Service, Interfaces, User Repository

---
+++ Backend/GuineaPigApp.Server/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using GuineaPigApp.Server.Database.Entities;
+
+namespace GuineaPigApp.Server.Interfaces
+{
+    public interface IUserRepository
+    {
+        User? GetUser(string email);
+        void AddUser(User user);
+        void RemoveUser(User user);
+        void UpdateUser(User user);
+    }
+}
---
+++ Backend/GuineaPigApp.Server/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using GuineaPigApp.Server.Database.Entities;
+using GuineaPigApp.Server.Database;
+using GuineaPigApp.Server.Interfaces;
+
+namespace GuineaPigApp.Server.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MyDbContext _context;
+
+        public UserRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+        public User? GetUser(string email)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == email);
+
+            return user;
+        }
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+        public void RemoveUser(User user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+        public void UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+    }
+}
