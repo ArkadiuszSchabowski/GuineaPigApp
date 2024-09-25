f04d983f66106ac408b6ac6104283e8618cf98bd
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-25 17:25:02 +0200

    Add tests for GenerateJWT and ChangePassword methods

--- Backend/GuineaPigApp.Server.Tests/UnitTests/Services/AccountServiceTests.cs
+++ Backend/GuineaPigApp.Server.Tests/UnitTests/Services/AccountServiceTests.cs
@@ -5,6 +5,7 @@ using GuineaPigApp.Server.Exceptions;
 using GuineaPigApp.Server.Interfaces;
 using GuineaPigApp.Server.Models;
 using GuineaPigApp.Server.Services;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 
 namespace GuineaPigApp.Server.Tests.UnitTests.Services
@@ -51,6 +52,69 @@ namespace GuineaPigApp.Server.Tests.UnitTests.Services
 
             var exception = Assert.Throws<BadRequestException>(action);
             Assert.Equal("Taki użytkownik nie istnieje!", exception.Message);
+        }
+        [Fact]
+        public void GenerateJWT_WhenValidLogin_ShouldGenerateJWTToken()
+        {
+            var mockUserRepository = new Mock<IUserRepository>();
+            var mockPasswordHasher = new Mock<IPasswordHasher<User>>();
+
+            var loginUserDto = new LoginUserDto()
+            {
+                Email = "Test@gmail.com",
+                Password = "Test123"
+            };
+
+            var user = new User()
+            {
+                Email = "Test@gmail.com",
+                PasswordHash = "###",
+                RoleId = 1
+            };
+
+            var testAuthenticationSettings = new AuthenticationSettings()
+            {
+                ExpireDays = 1,
+                JwtKey = "TestKey TestKey TestKey TestKey TestKey",
+                JwtIssuer = "Test Issuer"
+            };
+
+            var accountService = new AccountService(mockPasswordHasher.Object, null, testAuthenticationSettings, mockUserRepository.Object);
+
+            mockPasswordHasher.Setup(x => x.VerifyHashedPassword(user, user.PasswordHash, loginUserDto.Password)).Returns(PasswordVerificationResult.Success);
+
+            mockUserRepository.Setup(x => x.GetUser(loginUserDto.Email)).Returns(user);
+
+            var result = accountService.GenerateJWT(loginUserDto);
+
+            Assert.IsType<string>(result);
+        }
+
+        [Fact]
+        public void ChangePassword_WhenInvalidCurrentPassword_ShouldThrowBadRequestException()
+        {
+            var mockUserRepository = new Mock<IUserRepository>();
+            var mockPasswordHasher = new Mock<IPasswordHasher<User>>();
+
+            var accountService = new AccountService(mockPasswordHasher.Object, null, null, mockUserRepository.Object);
+
+            var dto = new ChangePasswordDto()
+            {
+                Email = "test@gmail.com",
+                CurrentPassword = "test1",
+                NewPassword = "test2",
+                RepeatNewPassword = "test2"
+            };
+
+            var user = new User();
+
+            mockUserRepository.Setup(x => x.GetUser(dto.Email)).Returns(user);
+            mockPasswordHasher.Setup(x => x.VerifyHashedPassword(user, user.PasswordHash, dto.CurrentPassword)).Returns(PasswordVerificationResult.Failed);
+
+            Action action = () => accountService.ChangePassword(dto);
+
+            var exception = Assert.Throws<BadRequestException>(action);
+            Assert.Equal("Wprowadzono niepoprawne hasło!", exception.Message);     
         }
     }
 }
