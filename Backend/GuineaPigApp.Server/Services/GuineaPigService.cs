9972228ecd3e9636028e0db2c7d3a1dac368abc4
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-26 22:38:39 +0200

    Implement method AddGuineaPig

--- Backend/GuineaPigApp.Server/Services/GuineaPigService.cs
+++ Backend/GuineaPigApp.Server/Services/GuineaPigService.cs
@@ -1,13 +1,47 @@
-﻿using GuineaPigApp.Server.Interfaces;
+﻿using AutoMapper;
+using GuineaPigApp.Server.Database;
+using GuineaPigApp.Server.Database.Entities;
+using GuineaPigApp.Server.Exceptions;
+using GuineaPigApp.Server.Interfaces;
 using GuineaPigApp.Server.Models;
 
 namespace GuineaPigApp.Server.Services
 {
     public class GuineaPigService : IGuineaPigService
     {
+        private readonly IGuineaPigRepository _guineaPigRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly MyDbContext _context;
+
+        public GuineaPigService(IGuineaPigRepository guineaPigRepository, IUserRepository userRepository, MyDbContext context, IMapper mapper)
+        {
+            _guineaPigRepository = guineaPigRepository;
+            _userRepository = userRepository;
+            _context = context;
+            _mapper = mapper;
+        }
         public void AddGuineaPig(string email, GuineaPigDto dto)
         {
-            throw new NotImplementedException();
+            User? user = _userRepository.GetUser(email);
+
+            if (user == null)
+            {
+                throw new BadRequestException("Taki użytkownik nie istnieje!");
+            }
+
+            var guineaPig = _guineaPigRepository.PigExists(user, dto.Name);
+
+            if (guineaPig)
+            {
+                throw new ConflictException("Posiadasz już taką świnkę morską o takim imieniu!");
+            }
+
+            var newGuineaPig = _mapper.Map<GuineaPig>(dto);
+
+            newGuineaPig.User = user;
+
+            _guineaPigRepository.AddGuineaPig(newGuineaPig);
         }
 
         public GuineaPigDto GetGuineaPig(string email, string name)
