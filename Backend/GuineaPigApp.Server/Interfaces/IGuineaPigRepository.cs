ddfc65f2dad18f8bcc2265526df38ed4eecd0560
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-27 14:55:38 +0200

    Change parameter from User to int in GetGuineaPigs method

--- Backend/GuineaPigApp.Server/Interfaces/IGuineaPigRepository.cs
+++ Backend/GuineaPigApp.Server/Interfaces/IGuineaPigRepository.cs
@@ -1,11 +1,12 @@
 ﻿using GuineaPigApp.Server.Database.Entities;
+using GuineaPigApp.Server.Models;
 
 namespace GuineaPigApp.Server.Interfaces
 {
     public interface IGuineaPigRepository
     {
         void AddGuineaPig(GuineaPig guineaPig);
-        List<GuineaPig> GetGuineaPigs(User user);
+        List<GuineaPig> GetGuineaPigs(int userId);
         bool PigExists(User user, string guineaPigName);
     }
 }
