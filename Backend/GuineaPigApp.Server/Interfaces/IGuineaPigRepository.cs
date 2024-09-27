e2148e1fa96cac2538e82aa32f730a313d3d9188
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-27 16:17:10 +0200

    Add GetGuineaPig method and contract for it.

--- Backend/GuineaPigApp.Server/Interfaces/IGuineaPigRepository.cs
+++ Backend/GuineaPigApp.Server/Interfaces/IGuineaPigRepository.cs
@@ -5,6 +5,7 @@ namespace GuineaPigApp.Server.Interfaces
     public interface IGuineaPigRepository
     {
         void AddGuineaPig(GuineaPig guineaPig);
+        GuineaPig? GetGuineaPig(int userId, string guineaPigName);
         List<GuineaPig> GetGuineaPigs(User user);
         bool PigExists(User user, string guineaPigName);
     }
