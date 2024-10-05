aee991b4290f484463101c92713e6f2a50af5bd2
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-05 18:26:09 +0200

    Update MaxLength value in data annotations, remove MinLength annotation

--- Backend/GuineaPigApp.Server.Database/Entities/Product.cs
+++ Backend/GuineaPigApp.Server.Database/Entities/Product.cs
@@ -7,13 +7,11 @@ namespace GuineaPigApp.Server.Database.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Nazwa produktu jest wymagana!")]
-        [MinLength(3, ErrorMessage = "Nazwa produktu nie może być krótsza niż 3 znaki!")]
         [MaxLength(25, ErrorMessage = "Nazwa produktu nie może być dłuższa niż 25 znaków!")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Opis produktu jest wymagany")]
-        [MinLength(15, ErrorMessage = "Opis produktu mnie może być krótszy niż 15 znaków!")]
-        [MaxLength(200, ErrorMessage = "Opis produktu nie może być dłuższy niż 200 znaków!")]
+        [MaxLength(1000, ErrorMessage = "Opis produktu nie może być dłuższy niż 1000 znaków!")]
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = "/assets/images/products/default.jpg";
         [Required]
