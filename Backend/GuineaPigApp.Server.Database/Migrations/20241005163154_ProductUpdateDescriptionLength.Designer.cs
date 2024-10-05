f710e7c07d57c2e6b9043e9ac949252321423ff1
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-05 18:32:46 +0200

    Add migration - Product Update Description Length

--- Backend/GuineaPigApp.Server.Database/Migrations/20241005163154_ProductUpdateDescriptionLength.Designer.cs
+++ Backend/GuineaPigApp.Server.Database/Migrations/20241005163154_ProductUpdateDescriptionLength.Designer.cs
@@ -4,6 +4,7 @@ using GuineaPigApp.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GuineaPigApp.Server.Database.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241005163154_ProductUpdateDescriptionLength")]
+    partial class ProductUpdateDescriptionLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
