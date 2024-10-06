4ac6559c6b040787ac8ab1cf27ea8839678ca96c
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-06 20:03:01 +0200

    Add PaginatorValidator with Interface for it

---
+++ Backend/GuineaPigApp.Server/Interfaces/IPaginatorValidator.cs
@@ -0,0 +1,9 @@
+﻿using GuineaPigApp.Server.Models;
+
+namespace GuineaPigApp.Server.Interfaces
+{
+    public interface IPaginatorValidator
+    {
+        void ValidatePagination(PaginationDto dto);
+    }
+}
