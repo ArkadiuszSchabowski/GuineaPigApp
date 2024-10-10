6adcbca833b45b4a9d4187694b3a45ad3826c505
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-10 22:10:17 +0200

    Add scoped UserService implementation with IUserService interface

--- Backend/GuineaPigApp.Server/Program.cs
+++ Backend/GuineaPigApp.Server/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IGuineaPigService, GuineaPigService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
