e86d07f9cd3a14aa6f437b33f5955f44d3f09bee
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-10 22:07:46 +0200

    Create Model: GetUserDto

---
+++ Backend/GuineaPigApp.Server/Models/GetUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GuineaPigApp.Server.Models
+{
+    public class GetUserDto
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string City {  get; set; } = string.Empty;
+    }
+}
