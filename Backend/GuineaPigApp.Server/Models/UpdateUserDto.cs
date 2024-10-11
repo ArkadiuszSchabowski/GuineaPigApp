1a06cdebffb1ee3e0dbaed9219f7c78d05c302b3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-11 16:53:14 +0200

    Add Model: UpdateUserDto

---
+++ Backend/GuineaPigApp.Server/Models/UpdateUserDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuineaPigApp.Server.Models
+{
+    public class UpdateUserDto
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Długość imienia nie może być krótsza niż 3 znaki!")]
+        [MaxLength(25, ErrorMessage = "Długość imienia nie może być dłuższa niż 25 znaków!")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(3, ErrorMessage = "Długość nazwiska nie może być krótsza niż 3 znaki!")]
+        [MaxLength(25, ErrorMessage = "Długość nazwiska nie może być dłuższa niż 25 znaków!")]
+        public string Surname { get; set; } = string.Empty;
+        [Required]
+        [MinLength(3, ErrorMessage = "Długość nazwy miasta nie może być krótsza niż 3 znaki!")]
+        [MaxLength(25, ErrorMessage = "Długość nazwy miasta nie może być dłuższa niż 25 znaków!")]
+        public string City { get; set; } = string.Empty;
+    }
+}
