449545643dc119fa5078344b2cfb644dcf6d245e
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-11 16:52:49 +0200

    Add UpdateUser method to IUserService

--- Backend/GuineaPigApp.Server/Interfaces/IUserService.cs
+++ Backend/GuineaPigApp.Server/Interfaces/IUserService.cs
@@ -5,5 +5,6 @@ namespace GuineaPigApp.Server.Interfaces
     public interface IUserService
     {
         GetUserDto GetUser(string email);
+        void UpdateUser(string email, UpdateUserDto dto);
     }
 }
