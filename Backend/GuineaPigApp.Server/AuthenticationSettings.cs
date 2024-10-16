9c37760a3c0284ecc8ef39340e022fdbf700ba0a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-16 23:03:27 +0200

    Set default token expiration to 30 days

--- Backend/GuineaPigApp.Server/AuthenticationSettings.cs
+++ Backend/GuineaPigApp.Server/AuthenticationSettings.cs
@@ -3,7 +3,7 @@
     public class AuthenticationSettings
     {
         public string JwtKey { get; set; } = string.Empty;
-        public int ExpireDays { get; set; }
+        public int ExpireDays { get; set; } = 30;
         public string JwtIssuer { get; set; } = string.Empty;
     }
 }
