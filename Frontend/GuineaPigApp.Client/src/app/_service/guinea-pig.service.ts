a5766b98d51cce4125e84ceb45f4fde93217f0a7
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-16 11:39:05 +0200

    Add authentication to addGuineaPig

--- Frontend/GuineaPigApp.Client/src/app/_service/guinea-pig.service.ts
+++ Frontend/GuineaPigApp.Client/src/app/_service/guinea-pig.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { GuineaPigDto } from '../_models/guinea-pig-dto';
@@ -10,6 +10,7 @@ import { environment } from '../_environments/environment_dev';
   providedIn: 'root',
 })
 export class GuineaPigService {
+  token: string | null = "";
   textSubject = new BehaviorSubject<string>('');
   isTextSubject$ = this.textSubject.asObservable();
 
@@ -44,9 +45,12 @@ export class GuineaPigService {
   addGuineaPig(email: string, guineaPigDto: GuineaPigDto) {
     let params = new HttpParams().set('email', email);
 
-    return this.http.post(environment.apiUrl + 'guineapig', guineaPigDto, {
-      params,
-    });
+    this.token = localStorage.getItem('token');
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+
+    return this.http.post(environment.apiUrl + 'guineapig', guineaPigDto, {params, headers},
+    );
   }
   updateWeight(email: string, guineaPig: GuineaPigDto) {
 
