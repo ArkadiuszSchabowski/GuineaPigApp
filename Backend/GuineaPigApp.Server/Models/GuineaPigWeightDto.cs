dd0f29bfcadf91f0427647f922d1b75e6bd0a288
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-10-27 21:02:31 +0100

    Add model: GuineaPigWeightDto

---
+++ Backend/GuineaPigApp.Server/Models/GuineaPigWeightDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuineaPigApp.Server.Models
+{
+    public class GuineaPigWeightDto
+    {
+        [Required]
+        [Range(50, 3000, ErrorMessage = "Waga świnki powinna wynosić od 50 do 3000 gram!")]
+        public int Weight { get; set; }
+    }
+}
