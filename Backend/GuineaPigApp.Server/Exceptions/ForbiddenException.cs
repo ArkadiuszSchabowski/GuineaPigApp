ff7b7f83c7584bbee5d6b48168160940e008ff57
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-11-03 12:39:14 +0100

    Add forbidden exception

---
+++ Backend/GuineaPigApp.Server/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace GuineaPigApp.Server.Exceptions
+{
+    public class ForbiddenException : Exception
+    {
+        public ForbiddenException(string message) : base(message)
+        {
+            
+        }
+    }
+}
