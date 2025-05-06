0b2bea4feaa540ea6492cfbc08ecc87cdeb77ba9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-05-06 16:20:06 +0200

    refactor: create GuineaPigValidator with interface

---
+++ Backend/GuineaPigApp.Server/Interfaces/IGuineaPigValidator.cs
@@ -0,0 +1,10 @@
+﻿using GuineaPigApp.Server.Database.Entities;
+
+namespace GuineaPigApp.Server.Interfaces
+{
+    public interface IGuineaPigValidator
+    {
+        void ThrowIfGuineaPigIsNull(GuineaPig? guineaPig);
+        void ThrowIfWeightIsNotCorrect(int weight);
+    }
+}
---
+++ Backend/GuineaPigApp.Server/Validators/GuineaPigValidator.cs
@@ -0,0 +1,25 @@
+﻿using GuineaPigApp.Server.Database.Entities;
+using GuineaPigApp.Server.Exceptions;
+using GuineaPigApp.Server.Interfaces;
+
+namespace GuineaPigApp.Server.Validators
+{
+    public class GuineaPigValidator : IGuineaPigValidator
+    {
+        public void ThrowIfGuineaPigIsNull(GuineaPig? guineaPig)
+        {
+            if (guineaPig == null)
+            {
+                throw new BadRequestException("Nie posiadasz świnki morskiej o takim imieniu!");
+            }
+        }
+
+        public void ThrowIfWeightIsNotCorrect(int weight)
+        {
+            if (weight < 50 || weight > 3000)
+            {
+                throw new BadRequestException("Waga świnki musi mieścić się w przedziale 50 do 3000gram!");
+            }
+        }
+    }
+}
