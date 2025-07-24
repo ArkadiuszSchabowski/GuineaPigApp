3b253f7eee6487b0049ceb519febdc0f197784d3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-24 15:45:14 +0200

    refactor: use reactive forms instead of template-driven forms

--- Frontend/GuineaPigApp_Client/src/app/components/public/login/login.component.spec.ts
+++ Frontend/GuineaPigApp_Client/src/app/components/public/login/login.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { ToastrService } from 'ngx-toastr';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from 'src/app/modules/angular-material/material.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -21,7 +21,7 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [AngularMaterialModule, BrowserAnimationsModule, FormsModule, HttpClientTestingModule],
+      imports: [AngularMaterialModule, BrowserAnimationsModule, ReactiveFormsModule, HttpClientTestingModule],
       providers: [{ provide: ToastrService, useClass: MockToastrService }],
     });
     fixture = TestBed.createComponent(LoginComponent);
